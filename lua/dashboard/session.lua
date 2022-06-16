d0f2598425b3dd525732dfb5e7de998fd8d830e5
Erven2016 <leiguihua2016@gmail.com>
2022-06-16 22:59:16 +0800

    fix(session): add support for Windows.
    changed the method of getting pwd and create directory; add two
    converters for directory and filename, and these will not be modified in
    Linux.

--- lua/dashboard/session.lua
+++ lua/dashboard/session.lua
@@ -1,28 +1,50 @@
-local fn,api = vim.fn,vim.api
+local fn, api = vim.fn, vim.api
 local db = require('dashboard')
 local session = {}
 local session_loaded = false
 
-local project_name = function()
-  local cwd = os.getenv('PWD')
-  return cwd
+function DirectoryConverter(directory)
+  -- replace '/' of db.session_directory to '\' for Windows
+  if vim.loop.os_uname().sysname == 'Windows_NT' then
+    return ((string.gsub(directory, '/', '\\')))
+  else
+    return directory
+  end
 end
 
+function FilenameConverter(filename)
+  -- replace '\' and 'C:\' to '_' and '' for Windows
+  if vim.loop.os_uname().sysname == 'Windows_NT' then
+    filename = ((string.gsub(filename, 'C:\\', '')))
+    return ((string.gsub(filename, '\\', '_')))
+  else
+    return filename
+  end
+end
+
+local project_name = function()
+  return FilenameConverter(vim.fn.getcwd())
+end
+
+local session_directory = DirectoryConverter(db.session_directory)
+
 function session.session_save(name)
-  if fn.isdirectory(db.session_directory) == 0 then
-    os.execute('mkdir -p' .. db.session_directory)
+  print(session_directory)
+  if fn.isdirectory(session_directory) == 0 then
+    vim.cmd(':!mkdir ' .. session_directory)
   end
 
   local file_name = name == nil and project_name() or name
-  local file_path = db.session_directory ..'/'..file_name ..'.vim'
-  api.nvim_command('mksession!'..fn.fnameescape(file_path))
+  local file_path = DirectoryConverter(session_directory .. '/' .. file_name .. '.vim')
+
+  api.nvim_command('mksession!' .. fn.fnameescape(file_path))
   vim.v.this_session = file_path
-  vim.notify('Session '..file_name .. ' is now persistent')
+  vim.notify('Session ' .. file_name .. ' is now persistent')
 end
 
 function session.session_load(name)
   local file_name = name == nil and project_name() or name
-  local file_path = db.session_directory .. '/' .. file_name ..'.vim'
+  local file_path = DirectoryConverter(session_directory .. '/' .. file_name .. '.vim')
 
   if vim.v.this_session ~= nil and not session_loaded then
     api.nvim_command('mksession! ' .. fn.fnameescape(vim.v.this_session))
@@ -36,15 +58,15 @@ function session.session_load(name)
       vim.opt.laststatus = 2
     end
 
-    vim.notify('Loaded '..file_path .. ' session')
+    vim.notify('Loaded ' .. file_path .. ' session')
     return
   end
 
-  vim.notify('The session '..file_path .. ' does not exist')
+  vim.notify('The session ' .. file_path .. ' does not exist')
 end
 
 function session.session_list()
-  return fn.split(db.session_directory..'/*.vim','\n')
+  return fn.split(session_directory .. '/*.vim', '\n')
 end
 
 return session
