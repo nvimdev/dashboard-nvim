2328102ea977cf85413f039efd3fcda138b10c47
Erven Lei <leiguihua2016@gmail.com>
2022-06-18 08:22:19 +0800

    fix(session): add support for Windows OS. (#157)
    * fix(session): add support for Windows.
    
    changed the method of getting pwd and create directory; add two
    converters for directory and filename, and these will not be modified in
    Linux.
    
    * test: try to fix filename error for Linux.
    
    Error executing Lua callback: ...ck/packer/start/dashboard-nvim/lua/dashboard/session.lu
    a:40: Vim(mksession):E190: Cannot open "/home/erven2016/.nvim_sessions//home/erven2016/.
    config/nvim.vim" for writing.
    
    * fix: sytax error when calling vim.loop.os_uame().
    
    * clean: deleted test printing.
    
    * fix: fix a nil value error and support Windows OS.
    
    This nil value was come from `local home = os.getenv("HOME")` that was
    changed to `local home = loop.os_homedir()`; Add some modifications of
    directory path for Windows OS.
    
    * fix: merge latest changes and use nvim native function to create folder.

--- lua/dashboard/session.lua
+++ lua/dashboard/session.lua
@@ -1,31 +1,49 @@
-local fn,api = vim.fn,vim.api
+local fn, api, loop = vim.fn, vim.api, vim.loop
 local db = require('dashboard')
 local session = {}
-local home = os.getenv("HOME")
+local home = loop.os_homedir()
+
+local isWindows = function()
+  if loop.os_uname().sysname == 'Windows_NT' then
+    return true
+  else
+    return false
+  end
+end
 
 local project_name = function()
   local cwd = fn.resolve(fn.getcwd())
-  cwd = fn.substitute(cwd,'^'..home..'/','','')
-  cwd = fn.fnamemodify(cwd,[[:p:gs?/?_?]])
-  cwd = fn.substitute(cwd,[[^\.]],'','')
+  cwd = fn.substitute(cwd, '^' .. home .. '/', '', '')
+  if isWindows() then
+    cwd = fn.fnamemodify(cwd, [[:p:gs?\?_?]])
+    cwd = ((string.gsub(cwd, 'C:', '')))
+  else
+    cwd = fn.fnamemodify(cwd, [[:p:gs?/?_?]])
+  end
+  cwd = fn.substitute(cwd, [[^\.]], '', '')
   return cwd
+end
+
+if isWindows() then
+  -- overwrite db.session_directory for Windows in this file
+  db.session_directory = string.gsub(db.session_directory, '/', '\\')
 end
 
 function session.session_save(name)
   if fn.isdirectory(db.session_directory) == 0 then
-    os.execute('mkdir -p ' .. db.session_directory)
+    fn.mkdir(db.session_directory, 'p')
   end
 
   local file_name = name == nil and project_name() or name
-  local file_path = db.session_directory ..'/'..file_name ..'.vim'
-  api.nvim_command('mksession! '..fn.fnameescape(file_path))
+  local file_path = db.session_directory .. '/' .. file_name .. '.vim'
+  api.nvim_command('mksession! ' .. fn.fnameescape(file_path))
   vim.v.this_session = file_path
-  vim.notify('Session '..file_name .. ' is now persistent')
+  vim.notify('Session ' .. file_name .. ' is now persistent')
 end
 
 function session.session_load(name)
   local file_name = name == nil and project_name() or name
-  local file_path = db.session_directory .. '/' .. file_name ..'.vim'
+  local file_path = db.session_directory .. '/' .. file_name .. '.vim'
 
   if vim.v.this_session ~= '' and fn.exists('g:SessionLoad') == 0 then
     api.nvim_command('mksession! ' .. fn.fnameescape(vim.v.this_session))
@@ -39,15 +57,16 @@ function session.session_load(name)
       vim.opt.laststatus = 2
     end
 
-    vim.notify('Loaded '..file_path .. ' session')
+    vim.notify('Loaded ' .. file_path .. ' session')
     return
   end
 
-  vim.notify('The session '..file_path .. ' does not exist')
+  vim.notify('The session ' .. file_path .. ' does not exist')
 end
 
 function session.session_list()
-   return vim.split(fn.globpath(db.session_directory,'*.vim'),'\n')
+  return vim.split(fn.globpath(db.session_directory, '*.vim'), '\n')
 end
 
 return session
+
