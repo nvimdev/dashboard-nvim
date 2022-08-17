44b397e7f796301671430396d9d71931147711b5
Raphael <glephunter@gmail.com>
2022-08-17 17:55:26 +0800

    better preview logic

--- lua/dashboard/events.lua
+++ lua/dashboard/events.lua
@@ -3,13 +3,24 @@ local au = {}
 local db = require('dashboard')
 local preview = require('dashboard.preview')
 
-local not_close = {
+local re_render = {
   ['packer'] = true,
   ['NvimTree'] = true,
   ['NeoTree'] = true,
-  ['dashboard'] = true,
-  ['dashboardpreview'] = true,
 }
+
+local function find_filetype()
+  local buffers = vim.fn.getbufinfo()
+  for _, buf in pairs(buffers) do
+    local filetype = api.nvim_buf_get_option(buf['bufnr'], 'filetype')
+    if re_render[filetype] then
+      return true
+    end
+  end
+  return false
+end
+
+local center_col = 0
 
 function au:dashboard_events()
   if not self.au_group then
@@ -19,21 +30,33 @@ function au:dashboard_events()
   api.nvim_create_autocmd({ 'BufWinEnter' }, {
     group = self.au_group,
     callback = function()
-      if not_close[vim.bo.filetype] then
+      if re_render[vim.bo.filetype] then
         if db.bufnr and api.nvim_buf_is_loaded(db.bufnr) then
           local winconfig = api.nvim_win_get_config(preview.winid)
-          winconfig['col'][false] = vim.o.columns / 1.4
+          center_col = winconfig['col'][false]
+          winconfig['col'][false] = center_col + 30
           api.nvim_win_set_config(preview.winid, winconfig)
         end
       end
 
-      if not not_close[vim.bo.filetype] then
+      if not re_render[vim.bo.filetype] then
         require('dashboard.preview'):close_preview_window()
         -- neovim-qt requires that conditional
         if self.au_group then
           api.nvim_del_augroup_by_id(self.au_group)
         end
         self.au_group = nil
+      end
+    end,
+  })
+
+  api.nvim_create_autocmd('BufEnter', {
+    group = self.au_group,
+    callback = function()
+      if vim.bo.filetype == 'dashboard' and not find_filetype() then
+        local winconfig = api.nvim_win_get_config(preview.winid)
+        winconfig['col'][false] = center_col
+        api.nvim_win_set_config(preview.winid, winconfig)
       end
     end,
   })
