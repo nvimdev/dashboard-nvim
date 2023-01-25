bc0886902f018466f7e00cf7183c05435a0bcc6e
Raphael <glephunter@gmail.com>
2023-01-25 18:31:34 +0800

    doom: support icon, icon_hi,desc_hi,key_hi

--- lua/dashboard/theme/doom.lua
+++ lua/dashboard/theme/doom.lua
@@ -8,7 +8,7 @@ local function generate_center(config)
       { desc = 'Please config your own center section', key = 'p' },
     })
   do
-    table.insert(lines, item.desc)
+    table.insert(lines, item.icon and item.icon .. item.desc or item.desc)
     table.insert(lines, '')
     if item.key then
       vim.keymap.set('n', item.key, function()
@@ -25,15 +25,36 @@ local function generate_center(config)
   local ns = api.nvim_create_namespace('DashboardDoom')
   local seed = 1
   for i = 1, #lines do
-    if lines[i]:find('%w') then
+    local scol = lines[i]:find('%w')
+    if scol then
       local idx = i == 1 and i or i - seed
       seed = seed + 1
-      api.nvim_buf_add_highlight(config.bufnr, 0, 'DashboardCenter', first_line + i - 1, 0, -1)
+      api.nvim_buf_add_highlight(
+        config.bufnr,
+        0,
+        config.center[idx].desc_hi or 'Number',
+        first_line + i - 1,
+        scol - 1,
+        -1
+      )
 
-      api.nvim_buf_set_extmark(config.bufnr, ns, first_line + i - 1, 0, {
-        virt_text_pos = 'eol',
-        virt_text = { { config.center[idx].key, 'Title' } },
-      })
+      if config.center[idx].icon then
+        api.nvim_buf_add_highlight(
+          config.bufnr,
+          0,
+          config.center[idx].icon_hi or 'String',
+          first_line + i - 1,
+          0,
+          scol - 1
+        )
+      end
+
+      if config.center[idx].key then
+        api.nvim_buf_set_extmark(config.bufnr, ns, first_line + i - 1, 0, {
+          virt_text_pos = 'eol',
+          virt_text = { { config.center[idx].key, config.center[idx].key_hi or 'Title' } },
+        })
+      end
     end
   end
 
