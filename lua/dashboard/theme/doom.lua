4cbcbf092e0d16dc93ea1f610ad8a4a27b0764df
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-03-08 19:33:55 +0800

    Added glepnir's suggestion.

--- lua/dashboard/theme/doom.lua
+++ lua/dashboard/theme/doom.lua
@@ -108,11 +108,7 @@ local function generate_center(config)
 
   local line = api.nvim_buf_get_lines(config.bufnr, first_line, first_line + 1, false)[1]
   local col = line:find('%w')
-  if col then
-    col = col - 1
-  else
-    col = 0
-  end
+  col = col and col - 1 or 0
   api.nvim_win_set_cursor(config.winid, { first_line + 1, col })
 
   local bottom = api.nvim_buf_line_count(config.bufnr)
