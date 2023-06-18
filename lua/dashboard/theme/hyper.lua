8baff69236fa49fe3fa7faa6433e4805694ff9fa
Alex S <hlcfan.yan@gmail.com>
2023-06-18 19:38:15 +0800

    Remove trailing null char (`\0`)

--- lua/dashboard/theme/hyper.lua
+++ lua/dashboard/theme/hyper.lua
@@ -108,6 +108,7 @@ local function project_list(config, callback)
 
   local function read_project(data)
     local res = {}
+    data = string.gsub(data, '%z', '')
     local dump = assert(loadstring(data))
     local list = dump()
     if list then
