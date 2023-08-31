c74212a4bba6adf187383797bdcb1c2ca3551d28
glepnir <glephunter@gmail.com>
2023-08-31 15:07:18 +0800

    remove lien2byte useage

--- plugin/dashboard.lua
+++ plugin/dashboard.lua
@@ -8,7 +8,11 @@ vim.g.loaded_dashboard = 1
 vim.api.nvim_create_autocmd('UIEnter', {
   group = vim.api.nvim_create_augroup('Dashboard', { clear = true }),
   callback = function()
-    if vim.fn.argc() == 0 and vim.fn.line2byte('$') == -1 then
+    if
+      vim.fn.argc() == 0
+      and vim.api.nvim_buf_line_count(0) == 1
+      and vim.api.nvim_buf_get_lines(0, 0, -1, false)[1] == ''
+    then
       require('dashboard'):instance()
     end
   end,
