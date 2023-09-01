36fb5382c4ed5d5e3625c1322bdfaaaea648a9b7
glepnir <glephunter@gmail.com>
2023-09-01 15:59:57 +0800

    fix create a new buffer

--- lua/dashboard/init.lua
+++ lua/dashboard/init.lua
@@ -205,8 +205,6 @@ function db:load_theme(opts)
     change_to_vcs_root = opts.change_to_vcs_root,
   })
 
-  -- api.nvim_buf_set_name(self.bufnr, utils.gen_bufname(opts.buffer_name))
-
   if #opts.preview.command > 0 then
     config = vim.tbl_extend('force', config, opts.preview)
   end
@@ -252,7 +250,7 @@ function db:instance()
     return
   end
 
-  if vim.fn.line2byte('$') ~= -1 then
+  if not utils.buf_is_empty(0) then
     self.bufnr = api.nvim_create_buf(false, true)
   else
     self.bufnr = api.nvim_get_current_buf()
--- lua/dashboard/utils.lua
+++ lua/dashboard/utils.lua
@@ -166,4 +166,10 @@ function utils.gen_bufname(prefix)
   return prefix .. '-' .. index
 end
 
+function utils.buf_is_empty(bufnr)
+  bufnr = bufnr or 0
+  return vim.api.nvim_buf_line_count(0) == 1
+    and vim.api.nvim_buf_get_lines(0, 0, -1, false)[1] == ''
+end
+
 return utils
