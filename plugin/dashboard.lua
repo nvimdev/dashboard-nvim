a893f3a92763abf3fecc99857364aab49c060940
Abao Zhang <abaodoit@gmail.com>
2024-03-26 17:16:17 +0800

    fix: restore empty file show dashboard

--- plugin/dashboard.lua
+++ plugin/dashboard.lua
@@ -8,11 +8,7 @@ vim.g.loaded_dashboard = 1
 vim.api.nvim_create_autocmd('UIEnter', {
   group = vim.api.nvim_create_augroup('Dashboard', { clear = true }),
   callback = function()
-    if
-      vim.fn.argc() == 0
-      and vim.api.nvim_buf_line_count(0) == 1
-      and vim.api.nvim_buf_get_lines(0, 0, -1, false)[1] == ''
-    then
+    if vim.fn.argc() == 0 and vim.api.nvim_buf_get_name(0) == '' then
       require('dashboard'):instance()
     end
   end,
